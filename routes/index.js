de063201efc578db2c3820fd2d85cc1e116f00cd
airasheed <abdullah@airasheed.com>
2015-10-05 16:01:24 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+/*
+* GET home page
+* */
+
+
+var express = require('express');
+
+var router = express.Router();
+
+/* GET home page*/
+
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Socket.io + Express = <3'});
+});
+
+module.exports = router;
